e3b4c3b6b181925f7f009ff8856db20c85b7dad4
vifid <vitor.gabrielcoelhofidelis@gmail.com>
2024-05-02 18:35:56 +0100

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/matchstream/MatchstreamApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.matchstream;
+
+import br.com.alura.matchstream.model.DadosSerie;
+import br.com.alura.matchstream.service.ConsumoApi;
+import br.com.alura.matchstream.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchstreamApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchstreamApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=88cb1409");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/test/java/br/com/alura/matchstream/MatchstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.matchstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
